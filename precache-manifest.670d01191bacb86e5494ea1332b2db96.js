a559b2438fd08f0f6213f2436e0f12e96181d900
Evgenia Kucherenko <evgeniakuzbit@gmail.com>
2021-08-28 17:18:30 +0300

    Updates

---
+++ precache-manifest.670d01191bacb86e5494ea1332b2db96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f73ecff3c2a14f1eb948",
+    "url": "/todo-list-react/static/css/main.09786286.chunk.css"
+  },
+  {
+    "revision": "f73ecff3c2a14f1eb948",
+    "url": "/todo-list-react/static/js/main.ef39a29f.chunk.js"
+  },
+  {
+    "revision": "35e3544cd546257cfadf",
+    "url": "/todo-list-react/static/js/runtime~main.bb4d8c92.js"
+  },
+  {
+    "revision": "de1920bda53434ba8e07",
+    "url": "/todo-list-react/static/css/2.b2f97ee7.chunk.css"
+  },
+  {
+    "revision": "de1920bda53434ba8e07",
+    "url": "/todo-list-react/static/js/2.b9b63945.chunk.js"
+  },
+  {
+    "revision": "07c5f12ca4869615018a8baffc2bac65",
+    "url": "/todo-list-react/index.html"
+  }
+];
